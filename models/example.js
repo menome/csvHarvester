3e3896b845878689ddc3012c3e6af9c786b50c8b
Mbarley <mbarley66@gmail.com>
2019-07-11 10:13:31 -0600

    Update example.js

--- models/example.js
+++ models/example.js
@@ -2,45 +2,45 @@ module.exports = {
     transform
   } 
 
-  function transform(itm){
-    return {
-      "Name": itm.CASENAME,
-      "NodeType":"MineralRightsClaim",
-      "SourceSystem": "CSVHarvester",
-      "Priority": 1,
-      "ConformedDimensions": {
-        "ObjectID":itm.OBJECTID
-      },
-      "Properties": {
-        "SubsurfaceRightsID":itm.SUBSURFACERIGHTID,
-        "LocalCaseID":itm.LOCALCASEID,
-        "RightName":itm.RIGHTNAME,
-        "ExpirationDate":itm.EXPIRATIONDATE,
-        "ActionDate":itm.ACTIONDATE,
-        "ActionCalYear":itm.ACTIONCALYEAR,
-        "ActionFiscalYear":itm.ACTIONFISCALYEAR,
-        "Comments":itm.COMMENTS,
-        "Region":itm.REGION,
-        "OfficialAcres":itm.OFFICIAL_ACRES,
-        "GISAcres":itm.GIS_ACRES,
-        "Minerals":itm.MINERALS,
-        "NFS_LANDUNIT_FK":itm.NFS_LANDUNIT_FK,
-        "ShapeLength":itm.SHAPE_Length,
-        "ShapeArea":itm.SHAPE_Area
-  },
-  "Connections":[
-    {
-      "NodeType": "ClaimantType",
-      "RelType": "CLAIMANT_TYPE",
-      "ForwardRel": true,
-      "ConformedDimensions": {
-        "sectionId": itm.CLAIMANTTYPE
-      },
-      "Properties": {
-
-      }
+function transform(itm){
+  return {
+    "Name": itm.CASENAME,
+    "NodeType":"MineralRightsClaim",
+    "SourceSystem": "CSVHarvester",
+    "Priority": 1,
+    "ConformedDimensions": {
+      "ObjectID":itm.OBJECTID
     },
-    {
+    "Properties": {
+      "SubsurfaceRightsID":itm.SUBSURFACERIGHTID,
+      "LocalCaseID":itm.LOCALCASEID,
+      "RightName":itm.RIGHTNAME,
+      "ExpirationDate":itm.EXPIRATIONDATE,
+      "ActionDate":itm.ACTIONDATE,
+      "ActionCalYear":itm.ACTIONCALYEAR,
+      "ActionFiscalYear":itm.ACTIONFISCALYEAR,
+      "Comments":itm.COMMENTS,
+      "Region":itm.REGION,
+      "OfficialAcres":itm.OFFICIAL_ACRES,
+      "GISAcres":itm.GIS_ACRES,
+      "Minerals":itm.MINERALS,
+      "NFS_LANDUNIT_FK":itm.NFS_LANDUNIT_FK,
+      "ShapeLength":itm.SHAPE_Length,
+      "ShapeArea":itm.SHAPE_Area
+    },
+    "Connections":[
+      {
+        "NodeType": "ClaimantType",
+        "RelType": "CLAIMANT_TYPE",
+        "ForwardRel": true,
+        "ConformedDimensions": {
+          "sectionId": itm.CLAIMANTTYPE
+        },
+        "Properties": {
+
+        }
+      },
+      {
         "NodeType": "CaseClass",
         "RelType": "CASE_CLASS",
         "ForwardRel": true,
@@ -73,6 +73,6 @@ module.exports = {
   
         }
       }
-  ]
- }
+    ]
+  }
 }
