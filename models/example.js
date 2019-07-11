cfc37d3805a37e435deab39bbac42fb8f9feb38a
mmorley <mmorley@menome.com>
2019-07-11 10:03:43 -0600

    updatd

--- models/example.js
+++ models/example.js
@@ -1,31 +1,78 @@
 module.exports = {
     transform
   } 
-  
+
   function transform(itm){
     return {
-      "Name": itm.name,
-      "NodeType":"Person",
+      "Name": itm.CASENAME,
+      "NodeType":"MineralRightsClaim",
       "SourceSystem": "CSVHarvester",
       "Priority": 1,
       "ConformedDimensions": {
-        "Code": itm.code
+        "ObjectID":itm.OBJECTID
+      },
+      "Properties": {
+        "SubsurfaceRightsID":itm.SUBSURFACERIGHTID,
+        "LocalCaseID":itm.LOCALCASEID,
+        "RightName":itm.RIGHTNAME,
+        "ExpirationDate":itm.EXPIRATIONDATE,
+        "ActionDate":itm.ACTIONDATE,
+        "ActionCalYear":itm.ACTIONCALYEAR,
+        "ActionFiscalYear":itm.ACTIONFISCALYEAR,
+        "Comments":itm.COMMENTS,
+        "Region":itm.REGION,
+        "OfficialAcres":itm.OFFICIAL_ACRES,
+        "GISAcres":itm.GIS_ACRES,
+        "Minerals":itm.MINERALS,
+        "NFS_LANDUNIT_FK":itm.NFS_LANDUNIT_FK,
+        "ShapeLength":itm.SHAPE_Length,
+        "ShapeArea":itm.SHAPE_Area
+  },
+  "Connections":[
+    {
+      "NodeType": "ClaimantType",
+      "RelType": "CLAIMANT_TYPE",
+      "ForwardRel": true,
+      "ConformedDimensions": {
+        "sectionId": itm.CLAIMANTTYPE
       },
       "Properties": {
 
-      },
-      "Connections":[
-        {
-          "NodeType": "Skill",
-          "RelType": "IN_SECTION",
-          "ForwardRel": true,
-          "ConformedDimensions": {
-            "sectionId": itm.skill
-          },
-          "Properties": {
-
-          }
+      }
+    },
+    {
+        "NodeType": "CaseClass",
+        "RelType": "CASE_CLASS",
+        "ForwardRel": true,
+        "ConformedDimensions": {
+          "sectionId": itm.CASECLASS
+        },
+        "Properties": {
+  
         }
-      ]
-    }
-  }
+      },
+      {
+        "NodeType": "RightRype",
+        "RelType": "RIGHT_TYPE",
+        "ForwardRel": true,
+        "ConformedDimensions": {
+          "sectionId": itm.RIGHTTYPE
+        },
+        "Properties": {
+  
+        }
+      },
+      {
+        "NodeType": "StatusMethod",
+        "RelType": "STATUS_METHOD",
+        "ForwardRel": true,
+        "ConformedDimensions": {
+          "sectionId": itm.STATUSMETHOD
+        },
+        "Properties": {
+  
+        }
+      }
+  ]
+ }
+}
