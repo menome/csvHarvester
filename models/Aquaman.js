58df49f3f034f4e9321fe934676ff7c2fed4d96b
Mbarley <mbarley66@gmail.com>
2019-09-23 13:21:43 -0600

    fix

---
+++ models/Aquaman.js
@@ -0,0 +1,79 @@
+module.exports = {
+    transform
+  } 
+  
+  function transform(itm){
+    return {
+      "Name": itm.Material,
+      "NodeType":"Valve",
+      "SourceSystem": "CSVHarvester",
+      "Priority": 1,
+      "ConformedDimensions": {
+        "SKU": itm.Material
+      },
+      "Properties": {
+        "Description": itm.Description,
+        "Available": itm.Available,
+        "LocationText":itm["Location Text"],
+        "TrackingCd":itm["Tracking Cd"],
+        "Text":itm.Text
+      },
+      "Connections":[
+        {
+          "NodeType": "UOM",
+          "RelType": "UOM",
+          "ForwardRel": true,
+          "ConformedDimensions": {
+            "UOM": itm.UOM
+          },
+          "Properties": {
+
+          }
+        },
+        {
+          "NodeType": "Area",
+          "RelType": "IN_AREA",
+          "ForwardRel": true,
+          "ConformedDimensions": {
+            "UOM": itm.Area
+          },
+          "Properties": {
+
+          }
+        },
+        {
+          "NodeType": "Section",
+          "RelType": "IN_SECTION",
+          "ForwardRel": true,
+          "ConformedDimensions": {
+            "UOM": itm.Section
+          },
+          "Properties": {
+
+          }
+        },
+        {
+          "NodeType": "Bin",
+          "RelType": "IN_BIN",
+          "ForwardRel": true,
+          "ConformedDimensions": {
+            "UOM": itm.Bin
+          },
+          "Properties": {
+
+          }
+        },
+        {
+          "NodeType": "Manufacturer",
+          "RelType": "MANUFACTURED_BY",
+          "ForwardRel": true,
+          "ConformedDimensions": {
+            "Name":itm["MANUFACTURER"]
+          },
+          "Properties": {
+    
+          }
+        }
+      ]
+    }
+  }
